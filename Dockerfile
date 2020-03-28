fba02c171a83017ccb240152dc5e4ef2385b5345
AnyKeyShik <nikitag594@gmail.com>
2020-03-28 14:48:22 +0300

    Fix env vars

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.8
 COPY . /Kektor3000
 WORKDIR /Kektor3000
 RUN pip3 install -r requirements.txt
-ENV Kektor3000_HOME /Kektor3000
+ENV KEKTOR3000_HOME /Kektor3000
 RUN mkdir /Kektor3000/logs
 CMD python ./run.py
--- utils/logger.py
+++ utils/logger.py
@@ -7,7 +7,7 @@ import os
 # Constants for logging
 _log_message_format = '%(levelname)s - %(asctime)-10s %(message)s'
 _message_format = 'Message "%s" from "%s" with answer "%s"'
-_message_log_filename = os.path.join(os.path.dirname(__file__), os.environ['BISMARCK_HOME'] + os.sep + "logs" + os.sep
+_message_log_filename = os.path.join(os.path.dirname(__file__), os.environ['KEKTOR3000_HOME'] + os.sep + "logs" + os.sep
                                      + "bismark_" + datetime.datetime.now().strftime("%Y_%m_%d_%H-%M-%S") + ".log")
 
 # Init logging
