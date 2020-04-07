fb9f292a02e02388257d0a3428e7a87c75470470
AnyKeyShik <nikitag594@gmail.com>
2020-04-07 04:03:16 +0300

    Fix typo

--- run.py
+++ run.py
@@ -7,13 +7,19 @@
 #  Telegram: @AnyKeyShik
 #  GitHub: https://github.com/AnyKeyShik
 #  E-mail: nikitav59@gmail.com
+#
+#  Created by AnyKeyShik Rarity
+#
+#  Telegram: @AnyKeyShik
+#  GitHub: https://github.com/AnyKeyShik
+#  E-mail: nikitav59@gmail.com
 
 from argparse import ArgumentParser
 
 from social import local_start, remote_start
 from utils import patch
 
-parser = ArgumentParser(description="Kektor bor")
+parser = ArgumentParser(description="Kektor bot")
 parser.add_argument("-p", "--proxy", help="Run bot with proxy", action='store_true')
 parser.add_argument("-l", "--local", help="Run bot locally", action='store_true')
 
